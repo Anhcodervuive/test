9f682b6b4f2448eead20572159f2e640f6a04c22
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2024-08-30 10:54:08 +0700

    from 2

--- resources/views/NoiThat360/index.blade.php
+++ resources/views/NoiThat360/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="w-full bg-blue-700">
-    <p class="text-gray-600">Content index page</p>
+    <p class="text-pink-600">Content index page</p>
 </div>
 
 @endsection
